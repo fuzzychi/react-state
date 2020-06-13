be3c3a4ed3c7e7059e1561e74052f1ff22b8bcbc
fuzzychi <dominance@gmail.com>
2020-06-13 13:12:36 -0400

    new

---
+++ src/Movies.js
@@ -0,0 +1,12 @@
+//http://www.omdbapi.com/?i=tt3896198&apikey=73b2396e
+//https://api.themoviedb.org/3/movie/550?api_key=766f9538cfe65a20e82986827e13778d
+
+const axios = require('axios').default;
+
+const movies =
+[
+    {movieId: "tt2543164"},
+    
+]
+
+export {movies}
